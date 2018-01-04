4660a08bd46852407a95350074855c87849b3c33
FrozenPandaz <jasonjean1993@gmail.com>
2018-01-03 22:43:49 -0500

    Demonstrate persistent App

--- server.ts
+++ server.ts
@@ -1,11 +1,13 @@
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
-import { renderModuleFactory } from '@angular/platform-server';
+import { platformServer, PlatformState, INITIAL_CONFIG } from '@angular/platform-server';
 import { enableProdMode } from '@angular/core';
 
 import * as express from 'express';
 import { join } from 'path';
 import { readFileSync } from 'fs';
+import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
+import { Router } from '@angular/router';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -22,37 +24,42 @@ const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toStri
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.bundle');
 
-// Express Engine
-import { ngExpressEngine } from '@nguniversal/express-engine';
-// Import module map for lazy loading
-import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
-
-// Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-app.engine('html', ngExpressEngine({
-  bootstrap: AppServerModuleNgFactory,
-  providers: [
+(async () => {
+  const platformRef = await platformServer([
+    {
+      provide: INITIAL_CONFIG,
+      useValue: {
+        document: template,
+        url: '/'
+      }
+    },
     provideModuleMap(LAZY_MODULE_MAP)
-  ]
-}));
+  ]).bootstrapModuleFactory(AppServerModuleNgFactory);
+  const platformState = platformRef.injector.get(PlatformState);
+  const router = platformRef.injector.get(Router);
 
-app.set('view engine', 'html');
-app.set('views', join(DIST_FOLDER, 'browser'));
+  app.set('view engine', 'html');
+  app.set('views', join(DIST_FOLDER, 'browser'));
 
-/* - Example Express Rest API endpoints -
-  app.get('/api/**', (req, res) => { });
-*/
+  /* - Example Express Rest API endpoints -
+    app.get('/api/**', (req, res) => { });
+  */
 
-// Server static files from /browser
-app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
-  maxAge: '1y'
-}));
+  // Server static files from /browser
+  app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
+    maxAge: '1y'
+  }));
 
-// ALl regular routes use the Universal engine
-app.get('*', (req, res) => {
-  res.render('index', { req });
-});
+  // ALl regular routes use the Universal engine
+  app.get('*', async (req, res) => {
+    // Navigate to the request's URL
+    await router.navigateByUrl(req.originalUrl);
+    // Serialize State after navigation completes
+    res.send(platformState.renderToString());
+  });
 
-// Start up the Node server
-app.listen(PORT, () => {
-  console.log(`Node Express server listening on http://localhost:${PORT}`);
-});
+  // Start up the Node server
+  app.listen(PORT, () => {
+    console.log(`Node Express server listening on http://localhost:${PORT}`);
+  });
+})();
